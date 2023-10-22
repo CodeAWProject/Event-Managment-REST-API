dc45c2a11922fcea148bb29a3fe682b0fdf411d1
CodeAWProject <activityware@gmail.com>
2023-10-22 19:41:24 +0200

    Adding action for showing single attendee with event id and attendee id

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -30,10 +31,10 @@ class AttendeeController extends Controller
         return new AttendeeResource($attendee);
     }
 
-    
-    public function show(string $id)
+    //This attendee will be scoped to an avent
+    public function show(Event $event, Attendee $attendee)
     {
-        //
+        return new AttendeeResource($attendee);
     }
 
     /**
