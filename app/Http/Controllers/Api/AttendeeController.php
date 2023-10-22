cddd36eff225153cda33cb49f88f10ecd9b04122
CodeAWProject <activityware@gmail.com>
2023-10-22 19:32:24 +0200

    Adding Attendee store action in AttendeeController

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -3,29 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    
+    public function index(Event $event)
     {
-        //
+        $attendees = $event->attendees()->latest();
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    
+    // It creates a new attendee and this will also automatically set the event_id 
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $event->attendees()->create([
+            'user_id' => 1
+        ]);
+
+        return new AttendeeResource($attendee);
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(string $id)
     {
         //
