8e72238e60501334345ce56d7075ceea9b6c15f5
CodeAWProject <activityware@gmail.com>
2023-10-22 17:10:57 +0200

    Adding delete action to event resource controller

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -66,6 +66,10 @@ class EventController extends Controller
      */
     public function destroy(Event $event)
     {
-        
+        $event->delete();
+
+        return response()->json([
+            'message' => 'Event deleted successfully'
+        ]);
     }
 }
